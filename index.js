a31e9daaa4fe8f7eed51645ada8efb2c3207ea75
iamyash337 <iamyash337@gmail.com>
2022-06-09 00:04:58 +0530

    main

--- index.js
+++ index.js
@@ -9,10 +9,8 @@ client.on('qr', qr => {
 });
 
 client.on('ready', () => {
-    console.log('HI YASH\nI M JARVIS');
+    console.log('HI YASH\n');
 });
-
-let info = "MADE BY - TEAM PROBOTS\nYASH SAHIL ATHARVA SUSHANT\n";
 
 client.initialize();
 
@@ -24,44 +22,71 @@ client.on('message', async message => {
     const cmd = message.body
     switch (cmd) {
         case 'Hi':
-            message.reply("HELLO\n\nI M JARVIS\n\nType Any\n\n1. Info\n2. Covid Updates\n3. Memes\n4. Memes On Programming\n5. Memes On Cricket\n6. Memes On Football\n7. Memes On Marvel\n")
+            message.reply("HELLO\n*I M JARVIS*😎\nType Any💻\n\n1 - INFO\n2 - TIME TABLE\n3 - COVID UPDATES\n4 - MEMES\n")
             break
 
         case '1':
-            message.reply(info)
+            message.reply("MADE BY - TEAM PROBOTS\n\nYASH - SAHIL - ATHARVA - SUSHANT\n");
             break
 
         case '2':
-            const info = await axios("https://coronavirus-19-api.herokuapp.com/countries/india")
+            // message.reply(FY_TT.jpeg);
+            const tt = await axios("O:\\YASH\\0\\GitHub\\WHATSAPP_BOT")
                 .then(res => res.data)
-            message.reply(`INDIA Has A Total Of : \*\*${info.cases}\*\*\ Covid Cases `,)
+            client.sendMessage(message.from, await MessageMedia.fromUrl(tt.url))
             break
 
         case '3':
-            const meme = await axios("https://meme-api.herokuapp.com/gimme/memes")
+            message.reply("Type Any💻\n\n21 - INDIA\n22 - US\n23 - CHINA\n24 - GERMANY\n")
+            break
+
+        case '31':
+            const a = await axios("https://coronavirus-19-api.herokuapp.com/countries/india")
                 .then(res => res.data)
-            client.sendMessage(message.from, await MessageMedia.fromUrl(meme.url))
+            message.reply(`INDIA Has A Total Of : ${a.cases} Covid Cases`)
+            break
+
+        case '32':
+            const b = await axios("https://coronavirus-19-api.herokuapp.com/countries/usa")
+                .then(res => res.data)
+            message.reply(`US Has A Total Of : ${b.cases} Covid Cases`)
+            break
+
+        case '33':
+            const c = await axios("https://coronavirus-19-api.herokuapp.com/countries/china")
+                .then(res => res.data)
+            message.reply(`CHINA Has A Total Of : ${c.cases} Covid Cases`)
+            break
+
+        case '34':
+            const d = await axios("https://coronavirus-19-api.herokuapp.com/countries/germany")
+                .then(res => res.data)
+            message.reply(`GERMANY Has A Total Of : ${d.cases} Covid Cases`)
             break
 
         case '4':
+            message.reply("Type Any💻\n\n31 - Memes On Programming\n32 - Memes On Cricket\n33 - Memes On Football\n34 - Memes On Marvel\n")
+            break
+
+        case '41':
             const memes = await axios("https://meme-api.herokuapp.com/gimme/ProgrammerHumor")
                 .then(res => res.data)
             client.sendMessage(message.from, await MessageMedia.fromUrl(memes.url))
             break
 
-        case '5':
+        case '42':
             const Cricket = await axios("https://meme-api.herokuapp.com/gimme/cricket")
                 .then(res => res.data)
             client.sendMessage(message.from, await MessageMedia.fromUrl(Cricket.url))
             break
 
-        case '6':
+        case '43':
             const Football = await axios("https://meme-api.herokuapp.com/gimme/football")
                 .then(res => res.data)
             client.sendMessage(message.from, await MessageMedia.fromUrl(Football.url))
             break
 
-        case '7':
+        case '44':
             const marvel = await axios("https://meme-api.herokuapp.com/gimme/marvelmemes")
                 .then(res => res.data)
             client.sendMessage(message.from, await MessageMedia.fromUrl(marvel.url))
