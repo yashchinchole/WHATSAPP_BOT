d1c6de4e071c71e803155150bb4c5de52ae0285c
iamyash337 <iamyash337@gmail.com>
2022-06-18 19:14:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ client.on('message', async message => {
             message.reply("HELLO\n*I M JARVIS* 😎\n\nMADE BY\n*YASH - SAHIL - ATHARVA - SUSHANT*\n\n*Type Any 💻*\n\n*1* - TECHFEST 2k22\n*2* - TIME TABLE\n*3* - COVID UPDATES\n*4* - MEMES\n")
             break
 
+        case 'hi':
+            message.reply("HELLO\n*I M JARVIS* 😎\n\nMADE BY\n*YASH - SAHIL - ATHARVA - SUSHANT*\n\n*Type Any 💻*\n\n*1* - TECHFEST 2k22\n*2* - TIME TABLE\n*3* - COVID UPDATES\n*4* - MEMES\n")
+            break
+
+        case 'HI':
+            message.reply("HELLO\n*I M JARVIS* 😎\n\nMADE BY\n*YASH - SAHIL - ATHARVA - SUSHANT*\n\n*Type Any 💻*\n\n*1* - TECHFEST 2k22\n*2* - TIME TABLE\n*3* - COVID UPDATES\n*4* - MEMES\n")
+            break
+
         case '1':
             message.reply("*Type Any 💻*\n\n*11* - Website\n*12* - About Events\n*13* - Registration\n*14* - Contacts\n");
             break
